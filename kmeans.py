3610dab25ce4e4857d94f15b9328759db5209836
Edwin Rachman <13515042@std.stei.itb.ac.id>
2018-10-21 13:38:37 +0700

    Add kmeans error

--- kmeans.py
+++ kmeans.py
@@ -11,8 +11,10 @@ class KMeans:
         self.iters = 0
         self.objects = None
         self.centroids = None
+        self.distance_matrix = None
         self.labels = None
         self.is_convergent = False
+        self.error = None
 
     def _calculate_centroids(self):
         sums = np.zeros((self.n_clusters, self.objects.shape[1]))
@@ -25,18 +27,22 @@ class KMeans:
     def fit(self, objects, init_centroids=None):
         self.iters = 0
         self.objects = objects
+        self.distance_matrix = None
         self.labels = np.full(len(self.objects), -1)
         self.is_convergent = False
+        self.error = None
         if init_centroids is None:
             self.centroids = self.objects[np.random.choice(len(self.objects), self.n_clusters, False), :]
         else:
             self.centroids = init_centroids
 
         while self.iters < self.max_iters and not self.is_convergent:
+            self.distance_matrix = cdist(self.centroids, self.objects, self.metric)
             prev_labels = self.labels
-            self.labels = cdist(self.centroids, self.objects, self.metric).argmin(0)
+            self.labels = self.distance_matrix.argmin(0)
             self.is_convergent = np.array_equal(prev_labels, self.labels)
             self.centroids = self._calculate_centroids()
+            self.error = np.sum(self.distance_matrix ** 2)
             self.iters += 1
 
     def fit_predict(self, objects, init_centroids=None):
